e98b180ae1badbc44fe06ba006280ac6f44b1099
Horvath Gergo <gege32@gmail.com>
2018-04-23 22:42:07 +0200

    quicksave

--- src/main/java/hu/gehorvath/szakdolgozat/droneadmin/Message.java
+++ src/main/java/hu/gehorvath/szakdolgozat/droneadmin/Message.java
@@ -25,9 +25,9 @@ public class Message {
 	private byte messageType;
 	
 	//value should be a 4 byte number in Q31 number format
-	private int value;
+	private byte[] value;
 
-	public Message(byte messageType, int value) {
+	public Message(byte messageType, byte[] value) {
 		super();
 		this.messageType = messageType;
 		this.value = value;
@@ -36,10 +36,10 @@ public class Message {
 	public void write(BufferedOutputStream os) throws IOException{
 		os.write(MESSAGE_START.getBytes(CHARSET));
 		os.write(messageType);
-		os.write((byte)(value >> 24));
-		os.write((byte)(value >> 16));
-		os.write((byte)(value >> 8));
-		os.write((byte)(value));
+		os.write((value[3]));
+		os.write((value[2]));
+		os.write((value[1]));
+		os.write((value[0]));
 		os.write(MESSAGE_END.getBytes(CHARSET));
 		os.flush();
 	}
---
+++ src/main/java/hu/gehorvath/szakdolgozat/droneadmin/Q31.java
@@ -0,0 +1,15 @@
+package hu.gehorvath.szakdolgozat.droneadmin;
+
+public class Q31 {
+	
+	//TODO: calculate
+	private static int SHIFT_NUMBER = 69;
+	
+	//this class represents the 32 bit q31 number format
+	private byte[] data = new byte[4];
+	
+	public Q31(float number) {
+		
+	}
+
+}
